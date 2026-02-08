0a9ca9704cc77323573cd76b38ec7fa9650e4549
b01tech <bruno01.tech@gmail.com>
2026-02-08 17:46:12 -0300

    feat: add Application layer service registration

---
+++ backend/src/Application/Extensions/ApplicationExtension.cs
@@ -0,0 +1,19 @@
+﻿using Application.Category.UseCases;
+using Application.Category.UseCases.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class ApplicationExtension
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        AddUseCases(services);
+        return services;
+    }
+
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<ICreateCategoryUseCase, CreateCategoryUseCase>();
+    }
+}
