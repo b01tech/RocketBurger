09fe60dace0610523540afecf402ffb8d85ce43d
b01tech <bruno01.tech@gmail.com>
2026-02-08 17:41:08 -0300

    feat(api): add endpoint extension for mapping API routes

---
+++ backend/src/Api/Extensions/EndpointExtension.cs
@@ -0,0 +1,12 @@
+﻿using Api.Endpoints;
+
+namespace Api.Extensions;
+
+public static class EndpointExtension
+{
+    public static void MapEndpoints(this IEndpointRouteBuilder app)
+    {
+        HealthApiEndpoint.Map(app);
+        CategoryEndpoint.Map(app);
+    }
+}
