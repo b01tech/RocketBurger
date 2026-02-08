ad8b719749b3444a8a3b3d9f5c4c076be01ef7d2
b01tech <bruno01.tech@gmail.com>
2026-02-08 17:40:55 -0300

    feat(category): add endpoint for creating categories

---
+++ backend/src/Api/Endpoints/CategoryEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using Application.Category.Dtos;
+using Application.Category.UseCases.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Endpoints;
+
+public static class CategoryEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+    {
+        app.MapGroup("api/categories").WithTags("Category").MapOpenApi();
+
+        app.MapPost(
+                "/",
+                async ([FromBody] CreateCategoryRequest request, [FromServices] ICreateCategoryUseCase useCase) =>
+                {
+                    var result = await useCase.ExecuteAsync(request);
+                    return result.IsSuccess ? Results.Ok(result.Data) : Results.BadRequest(result.Error);
+                }
+            )
+            .WithName("CreateCategory")
+            .WithSummary("Creates a new category");
+    }
+}
