6d52888a581e4a1bda035caa46f553bad562828d
b01tech <bruno01.tech@gmail.com>
2026-02-08 18:01:03 -0300

    feat(infra): add database infrastructure

---
+++ backend/src/Infra/Extensions/InfrastructureExtension.cs
@@ -0,0 +1,23 @@
+﻿using Infra.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Extensions;
+
+public static class InfrastructureExtension
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddAppDbContext(services, configuration);
+        return services;
+    }
+
+    private static void AddAppDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DbConnection") ?? string.Empty;
+
+        services.AddDbContext<RocketBugerDbContext>(options =>
+            options.UseSqlite(connectionString));
+    }
+}
