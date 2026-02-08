d4b623d0f32165b7709373f8a0b873fbf052b4e3
b01tech <bruno01.tech@gmail.com>
2026-02-08 17:34:41 -0300

    feat(category): add create category use case interface and implementation

---
+++ backend/src/Application/Category/UseCases/CreateCategoryUseCase.cs
@@ -0,0 +1,13 @@
+﻿using Application.Category.Dtos;
+using Application.Category.UseCases.Interfaces;
+using Core.Shared.Result;
+
+namespace Application.Category.UseCases;
+
+internal class CreateCategoryUseCase : ICreateCategoryUseCase
+{
+    public Task<Result<CategoryResponse>> ExecuteAsync(CreateCategoryRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ backend/src/Application/Category/UseCases/Interfaces/ICreateCategoryUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Application.Category.Dtos;
+using Core.Shared.Result;
+
+namespace Application.Category.UseCases.Interfaces;
+
+public interface ICreateCategoryUseCase
+{
+    Task<Result<CategoryResponse>> ExecuteAsync(CreateCategoryRequest request);
+}
