9a6573a8d8ce990d93c1b8ae0661b126950603c0
b01tech <bruno01.tech@gmail.com>
2026-02-08 17:29:37 -0300

    feat(docs): add Scalar for API documentation

---
+++ backend/src/Api/Extensions/DocumentationApiExtension.cs
@@ -0,0 +1,23 @@
+﻿using Scalar.AspNetCore;
+
+namespace Api.Extensions;
+
+public static class DocumentationApiExtension
+{
+    public static IServiceCollection AddDocumentationApi(this IServiceCollection services)
+    {
+        services.AddOpenApi();
+        return services;
+    }
+
+    public static void UseDocumentarionApi(this IEndpointRouteBuilder app)
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference("/docs", options =>
+        {
+            options.Title = "Rocket Burger API";
+            options.Theme = ScalarTheme.Solarized;
+        });
+
+    }
+}
--- backend/src/Api/Program.cs
+++ backend/src/Api/Program.cs
@@ -1,7 +1,11 @@
+using Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDocumentationApi();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseDocumentarionApi();
 
 app.Run();
