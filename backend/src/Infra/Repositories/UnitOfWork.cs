2ccced859b4461ea8598146fa98d43b7544a843d
b01tech <bruno01.tech@gmail.com>
2026-02-08 18:30:59 -0300

    feat(db): add unit of work pattern

---
+++ backend/src/Infra/Repositories/UnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Core.Repositories;
+using Infra.Data;
+
+namespace Infra.Repositories;
+
+internal class UnitOfWork(RocketBugerDbContext dbContext) : IUnitOfWork
+{
+    public async Task CommitAsync() => await dbContext.SaveChangesAsync();
+}
