3060b14dac8d0c32bf517d2610905b48914f16b2
b01tech <bruno01.tech@gmail.com>
2026-02-08 18:31:13 -0300

    feat: add category repository interface and implementation

---
+++ backend/src/Infra/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Core.Repositories;
+using Infra.Data;
+
+namespace Infra.Repositories;
+
+internal class CategoryRepository(RocketBugerDbContext dbContext) : ICategoryRepository
+{
+    public async Task<Category> AddCategoryAsync(Category category)
+    {
+        await dbContext.Categories.AddAsync(category);
+        return category;
+    }
+}
