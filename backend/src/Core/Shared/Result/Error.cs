f3f55ece42d1fa3286953db64b98b324dcffe1da
b01tech <bruno01.tech@gmail.com>
2026-02-08 17:28:14 -0300

    feat: add Result and Error

---
+++ backend/src/Core/Shared/Result/Error.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Shared.Result;
+
+public class Error
+{
+    public IEnumerable<string> Messages { get; set; } = [];
+    public int? Code { get; private set; } = 500;
+
+    public Error() { }
+    public Error(string message, int? code)
+    {
+        this.Messages = [message];
+        this.Code = code;
+    }
+
+    public Error(IEnumerable<string>errorMessages, int? code)
+    {
+        this.Messages = errorMessages;
+        Code = code;
+    }
+}
---
+++ backend/src/Core/Shared/Result/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.Shared.Result;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public T? Data { get; }
+    public Error? Error { get; }
+
+    private Result(bool isSuccess, T data, IEnumerable<string> errors, int? statusCode = null)
+    {
+        this.IsSuccess = isSuccess;
+        this.Data = data;
+        this.Error = new Error(errors, statusCode);
+    }
+
+    public static Result<T> Success(T data) => new Result<T>(true, data, default);
+
+    public static Result<T> Failure(IEnumerable<string> errors, int? statusCode = null) =>
+        new(false, default, errors, statusCode);
+
+    public static Result<T> Failure(string error, int? statusCode = null) =>
+        new(false, default, [error], statusCode);
+
+    public static implicit operator Result<T>(T data) => Success(data);
+}
