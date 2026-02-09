f6ad0468fc1fa00e4cd30714a88db56c8d39183a
b01tech <bruno01.tech@gmail.com>
2026-02-09 17:13:47 -0300

    feat(category): add update category use case

---
+++ backend/src/Application/Category/UseCases/Interfaces/IUpdateCategoryUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Application.Category.Dtos;
+using Core.Shared.Result;
+
+namespace Application.Category.UseCases.Interfaces;
+
+public interface IUpdateCategoryUseCase
+{
+    Task<Result<CategoryResponse>> ExecuteAsync(long id, UpdateCategoryRequest request);
+}
---
+++ backend/src/Application/Category/UseCases/UpdateCategoryUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Application.Category.Dtos;
+using Application.Category.UseCases.Interfaces;
+using Core.Repositories;
+using Core.Shared.Result;
+
+namespace Application.Category.UseCases;
+
+internal class UpdateCategoryUseCase(ICategoryRepository repository, IUnitOfWork unitOfWork) : IUpdateCategoryUseCase
+{
+    public async Task<Result<CategoryResponse>> ExecuteAsync(long id, UpdateCategoryRequest request)
+    {
+        var category = await repository.GetCategoryByIdAsync(id);
+        if (category is null)
+            return Result<CategoryResponse>.Failure("Categoria não encontrada");
+
+        category.Update(request.Description);
+        await unitOfWork.CommitAsync();
+        return new CategoryResponse(category.Id, category.Name, category.Description);
+    }
+}
