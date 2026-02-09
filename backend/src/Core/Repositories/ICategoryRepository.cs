972f977befc633c3a1f768c175ca3ff9cfc3d1a7
b01tech <bruno01.tech@gmail.com>
2026-02-09 16:54:28 -0300

    feat(category): add existence check before creating category

--- backend/src/Core/Repositories/ICategoryRepository.cs
+++ backend/src/Core/Repositories/ICategoryRepository.cs
@@ -4,5 +4,6 @@ namespace Core.Repositories;
 
 public interface ICategoryRepository
 {
+    Task<bool> CategoryExistsAsync(string name);
     Task<Category> AddCategoryAsync(Category category);
 }
--- backend/src/Infra/Repositories/CategoryRepository.cs
+++ backend/src/Infra/Repositories/CategoryRepository.cs
@@ -1,11 +1,17 @@
 ﻿using Core.Entities;
 using Core.Repositories;
 using Infra.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repositories;
 
 internal class CategoryRepository(RocketBugerDbContext dbContext) : ICategoryRepository
 {
+    public async Task<bool> CategoryExistsAsync(string name)
+    {
+        return await dbContext.Categories.AnyAsync(c => c.Name == name);
+    }
+
     public async Task<Category> AddCategoryAsync(Category category)
     {
         await dbContext.Categories.AddAsync(category);
