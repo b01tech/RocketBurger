9ce13130f3707963f38875a44c905cc3d9f7913f
b01tech <bruno01.tech@gmail.com>
2026-02-09 17:45:21 -0300

    test: add CategoryBuilder

---
+++ backend/test/TestUtilities/Builder/CategoryBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using Core.Entities;
+
+namespace TestUtilities.Builder;
+
+public class CategoryBuilder
+{
+    public static Category Build(string? inputName = null, string? inputDescription = null)
+    {
+        var faker = new Faker();
+        var name = inputName ?? faker.Commerce.Department();
+        var description = inputDescription ?? faker.Commerce.ProductAdjective();
+        var category = Category.Create(name, description);
+        return category.Data!;
+    }
+}
