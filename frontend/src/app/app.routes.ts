8470d4193aef4b1c4a42c9062e38ea24049fd489
b01tech <bruno01.tech@gmail.com>
2026-02-10 15:20:28 -0300

    feat: update main navigation and routing structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./features/product/pages/list-product-page/list-product-page').then(
+        (m) => m.ListProductPage,
+      ),
+  },
+  {
+    path: 'promotions',
+    loadComponent: () =>
+      import('./features/promotion/pages/promotion-page/promotion-page').then(
+        (m) => m.PromotionPage,
+      ),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./features/about/pages/about-page/about-page').then((m) => m.AboutPage),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contact/pages/contact-page/contact-page').then((m) => m.ContactPage),
+  },
+];
