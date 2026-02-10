6726cf17bd76466c091b18634e70144368075308
b01tech <bruno01.tech@gmail.com>
2026-02-10 08:42:17 -0300

    test: add request builders for category tests

---
+++ backend/test/TestUtilities/Builder/CreateCategoryRequestBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Application.Category.Dtos;
+using Bogus;
+
+namespace TestUtilities.Builder;
+
+public class CreateCategoryRequestBuilder
+{
+    public static CreateCategoryRequest Build(string? inputName = null, string? inputDescription = null)
+    {
+        var faker = new Faker();
+        var name = inputName ?? faker.Commerce.Department();
+        var description = inputDescription ?? faker.Commerce.ProductAdjective();
+        var categoryRequest = new CreateCategoryRequest(name, description);
+        return categoryRequest;
+    }
+}
---
+++ backend/test/TestUtilities/Builder/UpdateCategoryRequestBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Application.Category.Dtos;
+using Bogus;
+
+namespace TestUtilities.Builder;
+
+public class UpdateCategoryRequestBuilder
+{
+    public static UpdateCategoryRequest Build(string? inputDescription = null)
+    {
+        var faker = new Faker();
+        var description = inputDescription ?? faker.Commerce.ProductAdjective();
+        var categoryRequest = new UpdateCategoryRequest(description);
+        return categoryRequest;
+    }
+}
